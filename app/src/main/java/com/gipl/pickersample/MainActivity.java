26075b01e8d595cd6b2a08ad51f52a02a1a96e04
polekar.ankit <polekar.ankit@gmail.com>
2019-05-20 11:46:42 +0530

    resolve conflit in package name

--- app/src/main/java/com/gipl/pickersample/MainActivity.java
+++ app/src/main/java/com/gipl/pickersample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gipl.imagepicker;
+package com.gipl.pickersample;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -14,6 +14,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+
+import com.gipl.imagepicker.ImagePicker;
+import com.gipl.imagepicker.ImagePickerDialog;
+import com.gipl.imagepicker.PickerConfiguration;
+import com.gipl.imagepicker.PickerListener;
+import com.gipl.imagepicker.PickerResult;
 
 import java.io.File;
 
